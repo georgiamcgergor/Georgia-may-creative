727ecbe67457733c24eabab05dd6b8cffa3a5795
georgiamcgergor <72910513+georgiamcgergor@users.noreply.github.com>
2020-11-25 23:05:50 +1100

    Added js for nav

---
+++ resources/js/scripts.js
@@ -0,0 +1,10 @@
+function openNav () {
+    let toggler  = document.querySelector('.nav-prompt');
+    let navigation = document.querySelector ('.nav-inner' );
+
+    toggler.onclick = function (){
+        navigation.classList.toggle('open');
+    };
+}
+
+openNav() ; 
